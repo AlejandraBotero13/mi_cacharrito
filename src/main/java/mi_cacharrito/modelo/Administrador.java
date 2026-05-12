4e55d82f2f215e9662d6990d66ad84447670beec
Kevin Mauricio Roman Salazar <kevin.roman57170@ucaldas.edu.co>
2026-05-12 11:52:09 -0500

    Conexión con SQL

--- src/main/java/mi_cacharrito/modelo/Administrador.java
+++ src/main/java/mi_cacharrito/modelo/Administrador.java
@@ -2,15 +2,17 @@ package mi_cacharrito.modelo;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+ 
 @Entity
 @Table(name = "administrador")
 public class Administrador {
 
     @Id
-    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
 
     @Column(name="nombre", length=45, nullable=false)
