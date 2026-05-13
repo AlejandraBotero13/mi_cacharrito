e9ed246db4665365b6a137b856af1c557e14a73a
AlejandraBotero13 <diana.botero56520@ucaldas.edu.co>
2026-05-12 22:47:37 -0500

    Repositorio Automovil

--- src/main/java/mi_cacharrito/repositorio/administrador.java
+++ src/main/java/mi_cacharrito/repositorio/administrador.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import mi_cacharrito.modelo.Administrador;
 
 @Repository
-public interface administrador extends JpaRepository<Administrador, String>{
+public interface administrador extends JpaRepository<Administrador, Integer> {
 
 }
--- src/main/java/mi_cacharrito/repositorio/automovil.java
+++ src/main/java/mi_cacharrito/repositorio/automovil.java
@@ -1,5 +1,16 @@
 package mi_cacharrito.repositorio;
 
-public class automovil {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import mi_cacharrito.modelo.Automovil;
+import java.util.List;
+
+
+@Repository
+public interface automovil extends JpaRepository<Automovil, Integer> {
+    public List<Automovil> findByPlaca(String placa);
+    public List<Automovil> findByMarca(String marca);
+    public List<Automovil> findByModelo(String modelo);
+    public List<Automovil> findByCapacidad(String capacidad);
+
 }
