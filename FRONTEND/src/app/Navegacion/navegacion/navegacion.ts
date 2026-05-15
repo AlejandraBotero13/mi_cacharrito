d37337d46c92cd14da20efe6a838b486a8fbddbb
Kevin Mauricio Roman Salazar <kevin.roman57170@ucaldas.edu.co>
2026-05-15 18:29:48 -0500

    Creación de las carpetas entidad, servicio, navegación y usuario

---
+++ FRONTEND/src/app/Navegacion/navegacion/navegacion.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navegacion',
+  imports: [],
+  templateUrl: './navegacion.html',
+  styleUrl: './navegacion.css',
+})
+export class Navegacion {}
