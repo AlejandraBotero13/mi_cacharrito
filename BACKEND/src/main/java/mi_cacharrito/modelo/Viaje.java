e14bcc44d57c322f460aaebb68b83dfa28ad5ec0
AlejandraBotero13 <diana.botero56520@ucaldas.edu.co>
2026-05-16 16:22:58 -0500

    otro error corregido y funcional

--- BACKEND/src/main/java/mi_cacharrito/modelo/Viaje.java
+++ BACKEND/src/main/java/mi_cacharrito/modelo/Viaje.java
@@ -37,7 +37,7 @@ public class Viaje {
 
     @ManyToOne
     @JoinColumn(name = "automovil_id", nullable = false)
-    private Automovil automovil_id;
+    private Automovil automovil;
 
     public enum EstadoViaje {
         activo, cancelado, finalizado,
@@ -51,7 +51,7 @@ public class Viaje {
         this.horaSalida = horaSalida;
         this.precio = precio;
         this.estado = estado;
-        this.automovil_id = automovil;
+        this.automovil = automovil;
     }
 
 
@@ -96,11 +96,11 @@ public class Viaje {
 
 
     public Automovil getAutomovil() {
-         return automovil_id; }
+         return automovil; }
 
 
     public void setAutomovil(Automovil automovil) {
-         this.automovil_id = automovil; }
+         this.automovil = automovil; }
 
 
 }
