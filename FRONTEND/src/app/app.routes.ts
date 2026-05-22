3b60806f0c2555ac16b944f819812759f278518c
AlejandraBotero13 <diana.botero56520@ucaldas.edu.co>
2026-05-21 23:16:56 -0500

    Se creo la barra de navegación con todos lo accesos

--- FRONTEND/src/app/app.routes.ts
+++ FRONTEND/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Usuarios } from './Usuario/usuarios/usuarios';
+import { Viajes } from './Viaje/viajes/viajes';
+import { Reservas } from './Reserva/reservas/reservas';
+import { Itinerarios } from './Itinerario/itinerarios/itinerarios';
+import { Destinos } from './Destino/destinos/destinos';
+import { Automoviles } from './Automovil/automoviles/automoviles';
+import { Administradores } from './Administrador/administradores/administradores';
 
-export const routes: Routes = [];
+export const routes: Routes = [ 
+    {path: '', redirectTo: 'reservas', pathMatch: 'full'},
+    {path: 'usuarios', component: Usuarios},
+    {path: 'viajes', component: Viajes},
+    {path: 'reservas', component: Reservas},
+    {path: 'itinerarios', component: Itinerarios},
+    {path: 'destinos', component: Destinos},
+    {path: 'automoviles', component: Automoviles},
+    {path: 'administradores', component: Administradores},
+
+];
