dc0e61fb3559eda8b206799c5c15f1dc8f6dc471
AlejandraBotero13 <diana.botero56520@ucaldas.edu.co>
2026-05-22 00:36:29 -0500

    Se creo guardar usuario (FRONTED)

--- FRONTEND/src/app/Servicio/usuario-serv.ts
+++ FRONTEND/src/app/Servicio/usuario-serv.ts
@@ -1,6 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../Usuario/usuarios/usuarios';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class UsuarioServ {}
+export class UsuarioServ {
+
+  private url = 'http://localhost:8080/usuarios/u';
+
+  constructor(private http: HttpClient) {}
+
+  listarUsuarios(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(`${this.url}/listar`);
+  }
+
+  buscarPorCc(cc: string): Observable<Usuario> {
+    return this.http.get<Usuario>(`${this.url}/buscarCc`, { params: { cc } });
+  }
+
+  guardarUsuario(usuario: Usuario): Observable<Usuario> {
+    return this.http.post<Usuario>(`${this.url}/guardar`, usuario);
+  }
+
+  eliminarUsuario(cc: string): Observable<string> {
+    return this.http.delete(`${this.url}/eliminar`, { params: { cc }, responseType: 'text' });
+  }
+}
